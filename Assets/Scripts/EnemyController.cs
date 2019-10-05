5fddfc7f15dca9c7b8f39a3057168f2f107b3354
Mattias Aksli <mattias.aksli@gmail.com>
2019-10-05 15:33:36 +0300

    Added enemy spawning logic

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,20 +1,23 @@
-﻿using System.Collections;
+﻿using Doozy.Engine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public GameObject[] enemyPrefabs;
-    public Enemy[] enemies;
+    public Enemy[] enemyPrefabs;
+    public List<Enemy> enemies = new List<Enemy>();
+    public AllyController allyController;
+    public float enemyRadius;
+    private float angle = Mathf.PI / 3;
     void Start()
     {
-        
+        enemyRadius = allyController.allyRadius + 2.5f;
+        PositionEnemies();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void Combat()
     {
@@ -29,5 +32,41 @@ public class EnemyController : MonoBehaviour
         {
             e.RoundStart();
         }
+    }
+
+    public void PositionEnemies()
+    {
+        Dictionary<int, Enemy.enemyClass> enemyAndIndex = loadEnemyPosition();
+        foreach (KeyValuePair<int, Enemy.enemyClass> pair in enemyAndIndex)
+        {
+            int enemyIndex = pair.Key;
+            int enemyClass = (int)pair.Value;
+
+            float enemyAngle = angle * enemyIndex;
+            float x = Mathf.Sin(enemyAngle) * enemyRadius;
+            float z = Mathf.Cos(enemyAngle) * enemyRadius;
+
+            Enemy enemy = Instantiate(enemyPrefabs[enemyClass], transform, this);
+            UICanvas canvas = enemy.GetComponentInChildren<UICanvas>();
+            canvas.CanvasName = "Enemy--" + enemyIndex * 1000;
+            canvas.name = canvas.CanvasName;
+
+            enemy.transform.localPosition = new Vector3(x, 0, z);
+            enemy.transform.localRotation = Quaternion.Euler(0, enemyAngle * (180f / Mathf.PI) + 180f, 0);
+            enemy.sectorIndex = enemyIndex;
+            enemies.Add(enemy);
+        }
+    }
+
+    public Dictionary<int, Enemy.enemyClass> loadEnemyPosition()
+    {
+        Dictionary<int, Enemy.enemyClass> enemyAndIndex = new Dictionary<int, Enemy.enemyClass>
+        {
+            { 1, Enemy.enemyClass.Basic },
+            { 3, Enemy.enemyClass.Basic },
+            { 5, Enemy.enemyClass.Basic }
+        };
+
+        return enemyAndIndex;
     }
 }
