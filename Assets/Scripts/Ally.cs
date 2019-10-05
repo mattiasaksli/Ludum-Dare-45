7b979aaeeda81082bc406d1119f83eb87a6b31e2
Issi <markusaksli@hotmail.com>
2019-10-05 09:34:24 +0300

    Packages and organisation

---
+++ Assets/Scripts/Ally.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ally : MonoBehaviour
+{
+    public float maxHealth;
+    public float health;
+    public int sectorIndex;
+    public allyClass unitType=0;
+    public allyAttack attackType=0;
+    public AllyController controller;
+    Animation anim;
+    public enum allyClass
+    {
+        Knight,
+        Mage,
+        Priest
+    }
+    public enum allyAttack
+    {
+        Basic,
+        Spell1,
+        Spell2
+    }
+    void Start()
+    {
+        anim = this.GetComponentInChildren<Animation>();
+        controller = this.GetComponentInParent<AllyController>();
+    }
+    public void RoundStart()
+    {
+
+    }
+    public void Combat()
+    {
+
+    }
+    public void Damage(float hp)
+    {
+
+    }
+    void Attack(int unit,int attack)
+    {
+        switch (unitType)
+        {
+            case allyClass.Knight:
+                switch (attackType)
+                {
+                    case allyAttack.Basic:
+                        break;
+                }
+                break;
+        }
+    }
+}
