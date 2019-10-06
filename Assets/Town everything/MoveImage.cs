b953c3ea42b9361be1ddeda1596e3bc538a552a2
Mattias Aksli <mattias.aksli@gmail.com>
2019-10-06 17:01:33 +0300

    Tiny fixes made in MoveImage.cs

--- Assets/Town everything/MoveImage.cs
+++ Assets/Town everything/MoveImage.cs
@@ -1,6 +1,4 @@
 ﻿using Doozy.Engine.UI;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveImage : MonoBehaviour
@@ -8,189 +6,111 @@ public class MoveImage : MonoBehaviour
     public Animator animator;
     public float speed;
     private float w;
-	Vector3 startingpos;
     bool isCenter;
-	bool HeroChosen = false;
-	bool wantsToExit = false;
-	bool GoToBattleButtonPressed = false;
-	bool SettingsOpen = false;
-	bool FormationChosen = false;
+    bool HeroChosen = false;
+    bool wantsToExit = false;
+    bool GoToBattleButtonPressed = false;
+    bool SettingsOpen = false;
+    bool FormationChosen = false;
     GameObject canvas;
-	public UIPopup popup;
-	public UIPopup toBattlepopup;
-	public UIPopup settingspopup;
-	public UIPopup formationpopup;
-	public UIView townview;
+    public UIPopup popup;
+    public UIPopup toBattlepopup;
+    public UIPopup settingspopup;
+    public UIPopup formationpopup;
+    public UIView townview;
 
-
-
-    // Start is called before the first frame update
     void Start()
     {
         canvas = GameObject.Find("View - MainTownView");
         w = canvas.GetComponent<RectTransform>().rect.width;
-		
-		startingpos = gameObject.transform.localPosition;
-
-		
     }
 
-    // Update is called once per frame
     void Update()
     {
         animator.SetBool("InCenter", isCenter);
 
-		
-	
-		if (gameObject.transform.localPosition.x < 0)
-		{
-           gameObject.transform.localPosition += new Vector3(speed, 0f, 0f);
-		   
-		}
+        if (gameObject.transform.localPosition.x < 0)
+        {
+            gameObject.transform.localPosition += new Vector3(speed, 0f, 0f);
+        }
 
+        if (HeroChosen == false)
+        {
+            if (gameObject.transform.localPosition.x >= 0)
+            {
+                isCenter = true;
+                popup.Show();
+            }
+        }
 
-		
-        
-		if(HeroChosen == false)
-		{
-			if(gameObject.transform.localPosition.x >= 0) 
-			
-			{
+        if (FormationChosen == true)
+        {
+            formationpopup.Hide();
+        }
 
-				isCenter = true;
-				popup.Show();
-				
+        if (HeroChosen == true && FormationChosen == false)
+        {
+            formationpopup.Show();
+        }
 
-			}
+        if (HeroChosen == true)
+        {
+            popup.Hide();
+        }
 
-		}
+        if (HeroChosen == true && GoToBattleButtonPressed == false && FormationChosen == true)
+        {
+            toBattlepopup.Show();
+        }
 
-		if(FormationChosen == true){
-		
-			formationpopup.Hide();
+        if (GoToBattleButtonPressed == true)
+        {
+            toBattlepopup.Hide();
+        }
 
-		}
-		
-		if(HeroChosen == true && FormationChosen == false)
+        if (GoToBattleButtonPressed == true && HeroChosen == true)
+        {
+            isCenter = false;
+            townview.Hide();
 
-		{
-		
-			formationpopup.Show();
+            if (gameObject.transform.localPosition.x < w + 50)
+            {
+                gameObject.transform.localPosition += new Vector3(speed, 0f, 0f);
+            }
+        }
 
-
-		}
-
-		
-			
-		
-			
-
-		if (HeroChosen == true) 
-		{
-
-			popup.Hide();
-						
-		
-		}
-
-	
-		
-
-		
-
-
-		if(HeroChosen == true &&  GoToBattleButtonPressed == false && FormationChosen == true)
-		{
-			
-			toBattlepopup.Show();
-			
-
-		}
-        
-		if (GoToBattleButtonPressed == true) {
-			toBattlepopup.Hide();
-			
-		}
-		
-
-
-		
-		if(GoToBattleButtonPressed == true && HeroChosen == true)
-		{
-			isCenter = false;
-			townview.Hide();
-
-
-			if(gameObject.transform.localPosition.x <  w + 50)
-			{
-
-				gameObject.transform.localPosition += new Vector3(speed, 0f, 0f);
-
-			}
-					
-		}
-		
-
-		if(SettingsOpen == true) 
-		
-		{
-		
-			settingspopup.Show();
-
-		}else if(SettingsOpen == false)
-		
-		{
-
-			settingspopup.Hide();
-
-		}
-		
-
-        
-
+        if (SettingsOpen == true)
+        {
+            settingspopup.Show();
+        }
+        else if (SettingsOpen == false)
+        {
+            settingspopup.Hide();
+        }
     }
 
-	
-    
-	public void ExitHeroSelect() 
-	{
+    public void ExitHeroSelect()
+    {
+        HeroChosen = true;
+    }
 
-		HeroChosen = true;
+    public void ToBattle()
+    {
+        GoToBattleButtonPressed = true;
+    }
 
-	}
+    public void OpenSettings()
+    {
+        SettingsOpen = true;
+    }
 
+    public void ExitSettings()
+    {
+        SettingsOpen = false;
+    }
 
-	public void ToBattle()
-	{
-	
-		GoToBattleButtonPressed = true;
-
-	}
-
-	public void OpenSettings() 
-	
-	{
-	
-		SettingsOpen = true;
-
-	}
-
-
-	public void ExitSettings()
-
-	{
-	
-		SettingsOpen = false;
-
-	}
-	
-
-	public void ExitFormation()
-
-	{
-	
-		FormationChosen = true;
-
-	}
-
+    public void ExitFormation()
+    {
+        FormationChosen = true;
+    }
 }
