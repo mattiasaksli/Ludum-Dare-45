1ac2c6cc324a18fc938f70d40a7ef44eb3ce83b8
aleksander avastu <aleksander.avastu@gmail.com>
2019-10-07 16:13:45 +0300

    yeah

---
+++ Assets/Scenes/Main Menu/MainMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using Doozy.Engine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    bool SettingsOpen = false;
+    public UIPopup settingspopup; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SettingsOpen == true)
+        {
+            settingspopup.Show();
+        }
+    }
+    public void OpenSettings()
+    {
+        SettingsOpen = true;
+    }
+    public void ExitSettings()
+    {
+        SettingsOpen = false;
+    }
+}
